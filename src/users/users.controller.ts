97683fcb42bcb0a2dfdf7ba34f1649f3d8899685
TomasGiudici <giudicitomas1@gmail.com>
2024-06-06 18:47:04 -0300

    Commit feature1

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,4 +15,9 @@ export class UsersController {
     async create(@Body() user: User): Promise<void> {
       this.usersService.create(user);
     }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() user: Partial<User>): Promise<void> {
+      this.usersService.update(Number(id), user);
+    }
   }
