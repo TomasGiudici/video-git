4b93520c16b33c4b0bfbec4fc052c9397a8ddb29
TomasGiudici <giudicitomas1@gmail.com>
2024-06-06 19:58:12 -0300

    Comentarios release

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -20,7 +20,7 @@ export class UsersController {
     async update(@Param('id') id: string, @Body() user: Partial<User>): Promise<void> {
       this.usersService.update(Number(id), user);
     }
-
+    // comentario de produccion
     @Get(':id')
     async findOne(@Param('id') id: string): Promise<User> {
       return await this.usersService.findOne(Number(id));
